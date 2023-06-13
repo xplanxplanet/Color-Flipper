c116e86f5a4dc32af592970ca2c9655c5e00d045
Ezeuko Chimeremeze <ezeukochimeremeze@gmail.com>
2023-06-14 00:03:09 +0100

    modified:   app.js

--- app.js
+++ app.js
@@ -12,9 +12,8 @@ const btn = document.getElementById("randomcolorBtn");
 const color = document.querySelector("#color");
 
 btn.addEventListener("click", function() {
-    const randomNumber = randomColor();
-   document.body.style.backgroundColor = colors[randomNumber];
-   color.textContent = colors[randomNumber];
+   document.body.style.backgroundColor = colors[randomColor()];
+   color.textContent = colors[randomColor()];
 })
 
 function randomColor() {

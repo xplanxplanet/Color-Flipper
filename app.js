bf076d365034d410faabfa3e7e8ff212ac0812bd
Ezeuko Chimeremeze <ezeukochimeremeze@gmail.com>
2023-06-14 09:25:02 +0100

    modified:   app.js

--- app.js
+++ app.js
@@ -6,7 +6,17 @@ const colors = ["#F0F8FF", "#FAEBD7", "#00FFFF", "#7FFFD4",
 "#FF7F50", "#6495ED", "#FFF8DC", "#DC143C", "#00008B",
 "#A9A9A9", "#666666", "#BDB76B", "#8B008B", "#A9A9A9",
 "#00FFFF", "#006400", "#BDB76B", "#8B008B", "#556B2F",
-"#FF8C00", "#9932CC", "#8B0000", "#E9967A", "#8FBC8F"];
+"#FF8C00", "#9932CC", "#8B0000", "#E9967A", "#8FBC8F",
+"#FFFF00", "#9ACD32", "#F5F5F5", "#F5DEB3", "#EE82EE",
+"#40E0D0", "#FF6347", "#D8BFD8", "#008080", "#D2B48C",
+"#000080", "#BDB76B", "#8B008B", "#556B2F", "#FF8C00",
+"#9932CC", "#8B0000", "#E9967A", "#8FBC8F", "#FFFF00",
+"#9ACD32", "#F5F5F5", "#F5DEB3", "#EE82EE", "#40E0D0",
+"#FF6347", "#D8BFD8", "#008080", "#D2B48C", "#000080",
+"#BDB76B", "#8B008B", "#556B2F", "#FF8C00", "#9932CC",
+"#8B0000", "#E9967A", "#8FBC8F", "#FFFF00", "#9ACD32",
+"#F5F5F5", "#F5DEB3", "#EE82EE", "#40E0D0",
+"#FF6347", "#D8BFD8", "#008080", "#D2B48C", "#000080"];
 
 const btn = document.getElementById("randomcolorBtn");
 const color = document.querySelector("#color");
